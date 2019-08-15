91cf1a620dc7894a394a6738198150b282ddcce0
Reiner Lanz <mur1s.playground@gmail.com>
2019-08-15 09:41:22 +0200

    host based config for index

--- public/index.php
+++ public/index.php
@@ -2,5 +2,13 @@
 
 require "../vendor/reinerlanz/frame/src/Boot.php";
 
-$boot = new Frame\Boot("../app/config/app.json");
+$host = $_SERVER['HTTP_HOST'];
+
+if ($host == "development.localhost") {
+    $cfg = "development";
+} else {
+    $cfg = "live";
+}
+
+$boot = new Frame\Boot("../app/config/app.{$cfg}.json");
 $boot->run();
